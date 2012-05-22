4264522d14ad3226f51b2d9b60b63ac980aba584
Adam Groszer <agroszer@mail.com>
2012-05-22 12:46:49 +0000

    Back to development: 0.1.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
 
 setup(
     name='m01.mongofake',
-    version='0.1.0',
+    version='0.1.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description="Fake MongoDB implementation",
