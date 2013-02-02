cac20132aa1d1e82ac23d20fdf42fdf27f315d27
Adam Groszer <agroszer@mail.com>
2013-02-02 13:14:31 +0000

    Preparing release 0.2.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
 
 setup(
     name='m01.mongofake',
-    version='0.2.1dev',
+    version='0.2.1',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description="Fake MongoDB implementation",
