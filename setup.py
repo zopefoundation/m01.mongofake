d95feb1276056c103fa3e98562c2bd5433a6a1c7
Stephan Richter <stephan.richter@gmail.com>
2013-03-06 15:56:16 -0500

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
 
 setup(
     name='m01.mongofake',
-    version='1.0.0',
+    version='1.0.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description="Fake MongoDB implementation",
